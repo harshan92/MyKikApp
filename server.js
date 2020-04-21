728be30f49e8a763890dd46dc7d9c23f43ab43ac
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-04-21 13:35:53 +0530

    Setup express

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express=require('express');
+const bodyParser=require('body-parser');
+const ejs=require('ejs');
+const http=require('http');
+const container=require('./container');
+
+container.resolve(function(){
+    const app=SetupExpress();
+
+    function SetupExpress(){
+        const app=express();
+        const server=http.createServer(app);
+        server.listen(3000, function(){
+            console.log("Listening port 3000.");
+        });
+    }
+})
