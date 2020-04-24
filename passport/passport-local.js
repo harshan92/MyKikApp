fdf223fb6bb2487a37a10a5486fc92bb54cf357f
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-04-24 15:24:56 +0530

    =Added encryption to password

--- passport/passport-local.js
+++ passport/passport-local.js
@@ -28,7 +28,7 @@ passport.use('local.signup',new LocalStrategy({
         }
         const newUser=new User();
         newUser.username=req.body.username;
-        newUser.password=req.body.password;
+        newUser.password=newUser.encryptPassword(req.body.password);
         newUser.email=req.body.email;
         newUser.first_name=req.body.fname;
         newUser.last_name=req.body.lname;
