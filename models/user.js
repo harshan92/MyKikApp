667fd5ca43debd2af8b69e3f0b0d160a6dbe8fcf
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-04-24 14:40:16 +0530

    =Added passport local for signup

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 const userSchema=mongoose.Schema({
     username:{type:String, unique:true},
     first_name:{type:String},
-    first_name:{type:String},
+    last_name:{type:String},
     email:{type:String, unique:true},
     password:{type:String, default:''},
     userImage:{type:String, default:'default.png'},
---
+++ passport/passport-local.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const passport=require('passport');
+const User=require('../models/user');
+const LocalStrategy=require('passport-local').Strategy;
+
+passport.serializeUser((user, done)=>{
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done)=>{
+    User.findById(id,(err, user)=>{
+        done(err,user);
+    });
+});
+
+passport.use('local.signup',new LocalStrategy({
+    usernameField:'email',
+    passwordField:'password',
+    passReqToCallback:true
+}, (req, email, password, done)=>{
+    User.findOne({email:email}, (err, user)=>{
+        if(err){
+            return done(err);
+        }
+        if(user){
+            return done(null, false, req.flash('error','User with email already exist.'));
+        }
+        const newUser=new User();
+        newUser.username=req.body.username;
+        newUser.password=req.body.password;
+        newUser.email=req.body.email;
+        newUser.first_name=req.body.fname;
+        newUser.last_name=req.body.lname;
+
+        newUser.save((err)=>{
+            done(null, newUser);
+        })
+    })
+}));
