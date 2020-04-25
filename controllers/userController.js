05f53980c553699d550af572b1c5e397d6193434
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-04-25 21:28:41 +0530

    =Debug some errors in login

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,21 +5,31 @@ module.exports=function(_, passport, UserValidation){
         SetRouting:function(router){
             router.get('/', this.indexPage);
             router.get('/signup', this.getSignUp);
-            router.get('/home', this.homePage)
-            router.post('/signup',UserValidation.signupValidation, this.postSignUp)
+            router.get('/home', this.homePage);
+
+            // router.post('/', UserValidation.signinValidation, this.postSignIn);
+            router.post('/',UserValidation.signinValidation, this.postSignin)
+            router.post('/signup',UserValidation.signupValidation, this.postSignUp);
         },
         indexPage:function(req, res){
-            return res.render('index');
+            const errors=req.flash('error');
+            return res.render('index',  {title:'MyKik | LogIn', messages:errors, hasErrors:errors.length>0});
         },
         getSignUp:function(req, res){
             const errors=req.flash('error')
-            return res.render('signup', {title:'MyKik | Login', messages:errors, hasErrors:errors.length>0});
+            return res.render('signup', {title:'MyKik | SignUp', messages:errors, hasErrors:errors.length>0});
         },
+        postSignin:passport.authenticate('local.signin',{
+            successRedirect:'/home',
+            failRedirect:'/',
+            failureFlash:true
+        }),
         postSignUp:passport.authenticate('local.signup',{
             successRedirect:'/home',
             failRedirect:'/signup',
             failureFlash:true
         }),
+        
         homePage:function(req, res){
             return res.render('home');
         }
--- helpers/UserValidation.js
+++ helpers/UserValidation.js
@@ -25,6 +25,26 @@ module.exports=function(){
             .catch((err)=>{
                 return next();
             })
+        },
+        signinValidation:(req, res, next)=>{
+            req.checkBody('email', 'Email is required.').notEmpty();
+            req.checkBody('email', 'Email is invalid.').isEmail();
+            req.checkBody('password', 'Password is required.').notEmpty();
+
+            req.getValidationResult()
+            .then((result)=>{
+                const errors=result.array();
+                const  messages=[];
+                errors.forEach(error => {
+                    messages.push(error.msg);
+                });
+                req.flash('error', messages);
+                res.redirect('/');
+            })
+            .catch((err)=>{
+                return next();
+            })
         }
+
     }
 }
