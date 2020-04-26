02a9199431e9477fdfedba6ee24c51964277a19d
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-04-26 15:45:21 +0530

    added and updated dependencies2

--- passport/passport-local.js
+++ passport/passport-local.js
@@ -40,12 +40,12 @@ passport.use('local.signup',new LocalStrategy({
 }));
 
 // signin
-passport.use('local.signin',new LocalStrategy({
+passport.use('local.signin', new LocalStrategy({
     usernameField:'email',
     passwordField:'password',
     passReqToCallback:true
 }, (req, email, password, done)=>{
-    User.findOne({email:email}, (err, user)=>{
+    User.findOne({'email':email}, (err, user)=>{
         if(err){
             return done(err);
         }
