6846940b5a6bd5f17f6a93d07cfe49b6f97851e4
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-05-13 21:29:44 +0530

    Added async and load all group rec

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,23 @@
 
-module.exports=function(){
+module.exports=function(async, Group, _){
     return {
         SetRouting:function(router){
             router.get('/home', this.homePage);
         },
         homePage:function(req, res){
-            return res.render('home');
+            async.parallel([
+                function(callback){
+                    Group.find({},(err, result)=>{
+                        callback(err, result);
+                    })
+                }
+            ], (err, result)=>{
+                const res1=result[0];
+                console.log(res1);
+
+                res.render('home', {title:'MyKik - Home',data:res1});
+            })
+             
         }
     }
 }
