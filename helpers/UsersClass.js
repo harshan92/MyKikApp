e6863df42484caf1c941835927e32a0ba449ed7c
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-05-20 15:27:31 +0530

    Defined remove user method

--- helpers/UsersClass.js
+++ helpers/UsersClass.js
@@ -9,6 +9,21 @@ class Users{
         return users;
     }
 
+    removeUser(id){
+        var user=this.getUser(id);
+        if(user){
+            this.users=this.users.filter((user)=> user.id !== id);
+        }
+        return user;
+    }
+
+    getUser(id){
+        var user=this.users.filter((userId)=>{
+            return userId.id===id;
+        })[0];
+        return user;
+    }
+
     getUsersList(room){
         var users=this.users.filter((user)=>user.room===room);
 
