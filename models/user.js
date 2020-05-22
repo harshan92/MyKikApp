5c3b36bc1e14abb06791fcd314bf9c97bd6609ae
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-05-22 14:01:16 +0530

    add the friend requests fields to user modal

--- models/user.js
+++ models/user.js
@@ -12,7 +12,19 @@ const userSchema=mongoose.Schema({
     facebook:{type:String, default:''},
     facebookTokens:Array,
     google:{type:String, default:''},
-    googleTokens:Array
+    googleTokens:Array,
+    sentRequest:[{
+        username:{type:String, default:''}
+    }],
+    request:[{
+        userId:{type:mongoose.Schema.Types.ObjectId,ref:"User"},
+        username:{type:String, default:''}
+    }], 
+    friendsList:[{
+        friendId:{type:mongoose.Schema.Types.ObjectId,ref:"User"},
+        friendname:{type:String, default:''}
+    }],
+    totalRequest:{type:Number, default:0}
 });
 
 userSchema.methods.encryptPassword=function(password){
