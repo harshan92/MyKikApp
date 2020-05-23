936fd4dadab57f56fca488647864e08d0e2217bd
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-05-24 00:32:01 +0530

    set the friend request join event

--- public/js/group/send_request.js
+++ public/js/group/send_request.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     var socket=io();
+
+    var room=$('#groupName').val();
+    var sender=$('#sender').val();
+
+    socket.on('connect', function(){
+        var params={
+            sender:sender
+        };
+        socket.emit('joinRequest', params, function(){
+            console.log("Joined");
+        })
+    })
 })
